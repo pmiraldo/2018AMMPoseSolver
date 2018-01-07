b1a987442e0b73663a1eee5872e5969fad033cca
joao <jcampos@isr.tecnico.ulisboa.pt>
2018-01-07 19:46:51 +0000

    The GlobalPnPFunctionInfo class is being created

---
+++ include/opengv/optimization_tools/objective_function_tools/GlobalPnPFunctionInfo.hpp
@@ -0,0 +1,30 @@
+#ifndef GLOBALPNPFUNCTIONINFO_H
+#define GLOBALPNPFUNCTIONINFO_H
+
+#include <opengv/optimization_tools/objective_function_tools/ObjectiveFunctionInfo.hpp>
+#include <opengv/relative_pose/AbsoluteAdapterBase.hpp>
+#include <opegv/types.hpp>
+
+class GlobalPNPFunctionInfo : public ObjectiveFunctionInfo {
+public:
+  GlobalPNPFunctionInfo(const opengv::absolute_pose::AbsoluteAdapterBase & adapter );
+  ~GlobalPNPFunctionInfo();
+
+  double GlobalPNPFunctionInfo::objective_function_value(const opengv::rotation_t & rotation,
+                                          const opengv::translation_t & translation);
+  opengv::rotation_t GlobalPNPFunctionInfo::rotation_gradient(const opengv::rotation_t & rotation,
+							      const opengv::translation_t & translation);
+  opengv::translation_t GlobalPNPFunctionInfo::translation_gradient(const opengv::rotation_t & rotation,
+                                             const opengv::translation_t & translation);
+
+private:
+  Eigen::Matrixd3 Mt;
+  Eigen::MatrixXd Mrt;
+  Eigen::MatrixXd Mr; 
+
+  Eigen::Vector3d vt; 
+  Eigen::VectorXd vr;
+
+};
+
+#endif
---
+++ src/optimization_tools/objective_function_tools/GlobalPNPFunctionInfo.cpp
@@ -0,0 +1,44 @@
+#include <opengv/optimization_tools/GlobalPNPFunctionInfo.hpp>
+#include <Eigen/Dense>
+
+GlobalPNPFunctionInfo:: GlobalPNPFunctionInfo(const opengv::absolute_pose::AbsoluteAdapterBase & adapter ){
+
+  Eigen::Matrixd3 Mt = Eigen::Matrix3d::Zero(3,3);
+  Eigen::MatrixXd Mrt = Eigen::MatrixXd::Zero(9,3);
+  Eigen::MatrixXd Mr = Eigen::MatrixXd::Zero(9,9);
+
+  Eigen::Vector3d vt = Eigen::Vector3d::Zero(3,1);
+  Eigen::VectorXd vr = Eigen::VectorXd::Zero(9,1);
+
+  Eigen::Matrix3d id = Eigen::Matrix3d::Identity(3,3);
+
+  Eigen::Matrix<double,3,1> vi;
+  Eigen::Matrix<double,3,1> xi;
+  Eigen::Matrix<double,3,1> ci;
+  Eigen::Matrix3d Qi;
+  Eigen::Matrix3d Vi;
+  Eigen::MatrixXd Mrt_i = Eigen::MatrixXd::Zero(3,9);
+  for( int i = 0; i < (int) indices.size(); i++ )
+  {
+    vi = adapter.getCamRotation(indices[i]) * adapter.getBearingVector(indices[i]);
+    xi = adapter.getPoint(indices[i]);
+    ci = adapter.getCamOffset(indices[i]);
+
+    Vi = vi * vi.transpose() / (vi.transpose() * vi);
+    Qi = (id - Vi).transpose() * (id - Vi);
+    
+    Mt = Mt + Qi;
+    vt = vt - (2 * Qi * ci);
+    Mrt_i.block<1,3>(0,0) = Qi(0,0) * xi.transpose(); Mrt_i.block<1,3>(0,3) = Qi(0,1) * xi.transpose(); Mrt_i.block<1,3>(0,6) = Qi(0,2) * xi.transpose();
+    Mrt_i.block<1,3>(1,0) = Qi(1,0) * xi.transpose(); Mrt_i.block<1,3>(1,3) = Qi(0,1) * xi.transpose(); Mrt_i.block<1,3>(1,6) = Qi(0,2) * xi.transpose();
+    Mrt_i.block<1,3>(2,0) = Qi(2,0) * xi.transpose(); Mrt_i.block<1,3>(2,3) = Qi(0,1) * xi.transpose(); Mrt_i.block<1,3>(2,6) = Qi(0,2) * xi.transpose();
+
+    Mrt = Mrt + Mrt_i.transpose();
+  }
+
+}
+
+
+
+;
+GlobalPNPFunctionInfo:: ~GlobalPNPFunctionInfo(){};
