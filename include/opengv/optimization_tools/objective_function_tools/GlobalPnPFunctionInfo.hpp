4ed04c7d075cf01de04a607c7146733de4c93e08
João Campos <joaobcampos@hotmail..com>
2018-01-08 15:04:25 +0000

    The constructor of class GlobalPnPInfoFunction compiles. Time to check it on the test file

--- include/opengv/optimization_tools/objective_function_tools/GlobalPnPFunctionInfo.hpp
+++ include/opengv/optimization_tools/objective_function_tools/GlobalPnPFunctionInfo.hpp
@@ -2,23 +2,23 @@
 #define GLOBALPNPFUNCTIONINFO_H
 
 #include <opengv/optimization_tools/objective_function_tools/ObjectiveFunctionInfo.hpp>
-#include <opengv/relative_pose/AbsoluteAdapterBase.hpp>
-#include <opegv/types.hpp>
+#include <opengv/absolute_pose/AbsoluteAdapterBase.hpp>
+#include <opengv/types.hpp>
 
 class GlobalPNPFunctionInfo : public ObjectiveFunctionInfo {
 public:
   GlobalPNPFunctionInfo(const opengv::absolute_pose::AbsoluteAdapterBase & adapter );
   ~GlobalPNPFunctionInfo();
 
-  double GlobalPNPFunctionInfo::objective_function_value(const opengv::rotation_t & rotation,
+  /*  double GlobalPNPFunctionInfo::objective_function_value(const opengv::rotation_t & rotation,
                                           const opengv::translation_t & translation);
   opengv::rotation_t GlobalPNPFunctionInfo::rotation_gradient(const opengv::rotation_t & rotation,
 							      const opengv::translation_t & translation);
   opengv::translation_t GlobalPNPFunctionInfo::translation_gradient(const opengv::rotation_t & rotation,
-                                             const opengv::translation_t & translation);
+  const opengv::translation_t & translation);*/
 
 private:
-  Eigen::Matrixd3 Mt;
+  Eigen::Matrix3d Mt;
   Eigen::MatrixXd Mrt;
   Eigen::MatrixXd Mr; 
 
