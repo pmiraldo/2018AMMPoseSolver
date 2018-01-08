75a764fc31de0b8cce43178fde8ca95ced6fbec3
joao <jcampos@isr.tecnico.ulisboa.pt>
2018-01-08 10:12:47 +0000

    Carry on implementing the model

--- src/optimization_tools/objective_function_tools/GlobalPNPFunctionInfo.cpp
+++ src/optimization_tools/objective_function_tools/GlobalPNPFunctionInfo.cpp
@@ -18,6 +18,8 @@ GlobalPNPFunctionInfo:: GlobalPNPFunctionInfo(const opengv::absolute_pose::Absol
   Eigen::Matrix3d Qi;
   Eigen::Matrix3d Vi;
   Eigen::MatrixXd Mrt_i = Eigen::MatrixXd::Zero(3,9);
+  Eigen::MatrixXd Mr1_i = Eigen::MatrixXd::Zero(3,9);
+  Eigen::VectorXd Mr2_i = Eigen::VectorXd::Zero(1,9);
   for( int i = 0; i < (int) indices.size(); i++ )
   {
     vi = adapter.getCamRotation(indices[i]) * adapter.getBearingVector(indices[i]);
@@ -29,11 +31,20 @@ GlobalPNPFunctionInfo:: GlobalPNPFunctionInfo(const opengv::absolute_pose::Absol
     
     Mt = Mt + Qi;
     vt = vt - (2 * Qi * ci);
+
+    //Calculate Mrt
     Mrt_i.block<1,3>(0,0) = Qi(0,0) * xi.transpose(); Mrt_i.block<1,3>(0,3) = Qi(0,1) * xi.transpose(); Mrt_i.block<1,3>(0,6) = Qi(0,2) * xi.transpose();
-    Mrt_i.block<1,3>(1,0) = Qi(1,0) * xi.transpose(); Mrt_i.block<1,3>(1,3) = Qi(0,1) * xi.transpose(); Mrt_i.block<1,3>(1,6) = Qi(0,2) * xi.transpose();
-    Mrt_i.block<1,3>(2,0) = Qi(2,0) * xi.transpose(); Mrt_i.block<1,3>(2,3) = Qi(0,1) * xi.transpose(); Mrt_i.block<1,3>(2,6) = Qi(0,2) * xi.transpose();
+    Mrt_i.block<1,3>(1,0) = Qi(1,0) * xi.transpose(); Mrt_i.block<1,3>(1,3) = Qi(1,1) * xi.transpose(); Mrt_i.block<1,3>(1,6) = Qi(1,2) * xi.transpose();
+    Mrt_i.block<1,3>(2,0) = Qi(2,0) * xi.transpose(); Mrt_i.block<1,3>(2,3) = Qi(2,1) * xi.transpose(); Mrt_i.block<1,3>(2,6) = Qi(2,2) * xi.transpose();
 
     Mrt = Mrt + Mrt_i.transpose();
+
+    //Calculate Mr
+    Mr1_i.block<1,3>(0,0) = Vi(0,0) * xi.transpose();Mr1_i.block<1,3>(0,3) = Vi(0,1) * xi.transpose();Mr1_i.block<1,3>(0,6) = Vi(0,2) * xi.transpose();
+    Mr1_i.block<1,3>(1,0) = Vi(1,0) * xi.transpose();Mr1_i.block<1,3>(1,3) = Vi(1,1) * xi.transpose();Mr1_i.block<1,3>(1,6) = Vi(1,2) * xi.transpose();
+    Mr1_i.block<1,3>(2,0) = Vi(2,0) * xi.transpose();Mr1_i.block<1,3>(2,3) = Vi(2,1) * xi.transpose();Mr1_i.block<1,3>(2,6) = Vi(2,2) * xi.transpose();
+
+
   }
 
 }
